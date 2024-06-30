ecb7a297c199dd418350f6c5183d21601a07881c
n0deS <np.phung@gmx.de>
2024-06-30 16:04:17 +0200

    added missing 'data' in path

--- rename_script.py
+++ rename_script.py
@@ -4,7 +4,7 @@ seq_arr = [1, 3, 4, 6, 7, 10, 11, 12, 13, 14, 16]
 for i in range(1, 41):
     for j in seq_arr:
         # Define the folder path
-        folder_path = "endovis_2018/train/"+ str(i) +"/sam_features_h/seq" + str(j)
+        folder_path = "data/endovis_2018/train/"+ str(i) +"/sam_features_h/seq" + str(j)
         print("In " + folder_path)
         # Iterate over all files in the folder
         for filename in os.listdir(folder_path):
